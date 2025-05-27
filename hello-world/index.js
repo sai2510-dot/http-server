6959446cfdb68c79e5f4b103686f0ac30695cebb
sai2510-dot <saipraseeda2510@gmail.com>
2025-05-27 18:31:30 +0530

    Add files via upload

---
+++ hello-world/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Hello World server running at http://localhost:${port}/`);
+});
